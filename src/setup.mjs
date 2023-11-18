6aed62a617240b48fa2e321905320c079a941ea1
Zane Rockenbaugh <zane@liquid-labs.com>
2023-11-18 15:47:43 -0600

    lint formatting

--- src/setup.mjs
+++ src/setup.mjs
@@ -1,10 +1,6 @@
-import * as fsPath from 'node:path'
-
 import { npmPackageNameRE } from '@liquid-labs/regex-repo'
 
 import { IntegrationsManager } from './integrations-manager'
-import { pluginPackageDir } from './integrations/integration-controls'
-import { registerIntegrations } from './integrations/register-integrations'
 
 const setup = async({ app, reporter }) => {
   app.ext.setupMethods.push({
