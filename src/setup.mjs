ba176e4275e3a0f5b41d0e4ad8738ddd823a4446
Zane Rockenbaugh <zane@liquid-labs.com>
2023-11-18 15:46:29 -0600

    Remove integration plugin logic

--- src/setup.mjs
+++ src/setup.mjs
@@ -1,7 +1,4 @@
-import { existsSync } from 'node:fs'
 import * as fsPath from 'node:path'
-
-import findPlugins from 'find-plugins'
 
 import { npmPackageNameRE } from '@liquid-labs/regex-repo'
 
@@ -9,55 +6,12 @@ import { IntegrationsManager } from './integrations-manager'
 import { pluginPackageDir } from './integrations/integration-controls'
 import { registerIntegrations } from './integrations/register-integrations'
 
-const loadPlugins = async({ app, reporter }) => {
-  const pluginPath = pluginPackageDir({ app })
-  const pluginPkg = fsPath.join(pluginPath, 'package.json')
-  const pluginDir = fsPath.join(pluginPath, 'node_modules')
-
-  reporter.log(`Searching for integration plugins (in ${fsPath.dirname(pluginDir)})...`)
-
-  const pluginOptions = {
-    pkg    : pluginPkg, // will load dependencies as plugins
-    dir    : pluginDir, // will load from here
-    filter : () => true // every dependency is a plugin
-  }
-
-  const plugins = existsSync(pluginPkg) ? findPlugins(pluginOptions) : []
-
-  reporter.log(plugins.length === 0 ? 'No integration plugins found.' : `Found integration ${plugins.length} plugins.`)
-
-  for (const { dir, pkg } of plugins) {
-    const { main, name: npmName } = pkg
-    const { name = 'UNKNOWN', registerIntegrationPlugins } = await import(`${dir}/${main}`) || {}
-    if (registerIntegrationPlugins === undefined) {
-      throw new Error(`'liq-integrations' plugin from '${npmName}' does not export 'registerIntegrationPlugins'; bailing out.`)
-    }
-    reporter.log(`Registering '${npmName}' (${name})...`)
-
-    const registration = registerIntegrationPlugins({ app, reporter })
-    if (registration?.then !== undefined) {
-      await registration
-    }
-
-    app.ext._liqIntegrations.plugins.push({ npmName })
-  }
-}
-
 const setup = async({ app, reporter }) => {
-  app.ext._liqIntegrations = {
-    plugins : []
-  }
   app.ext.setupMethods.push({
-    name : 'setup and load integration plugins',
+    name : 'setup integrations',
     func : async({ app, reporter }) => {
       app.ext.integrations = new IntegrationsManager()
-      await loadPlugins({ app, reporter })
     }
-  },
-  {
-    name : 'register integration plugin integrations',
-    deps : ['setup and load integration plugins'],
-    func : registerIntegrations
   })
 
   setupPathResolvers({ app })
